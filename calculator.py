2e68ceb8eeebce07b6cd28f24a59200c5cd70560
Sabiha-dev-code <sabihajamil768@gmail.com>
2026-09-06 21:47:01 +0500

    Update calculator.py
    Fix input prompts and update print statements for clarity.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
-a=int(input("how many numbers are you gonna enter")
-      for i in range(a):
+a=int(input("enter number")
 b=int(input("enter a number")
-      print("you entered"b)
+print("sum",b+a)
+print(("subtract",a-b)
+print("divide",a/b)
       
       
